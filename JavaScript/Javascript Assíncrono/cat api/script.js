5f9c7f08bd55eecda7013873ccbb8553bd220c85
Guilherme <guilhermemsantana16@gmail.com>
2022-07-09 10:19:00 -0300

    Cat api

---
+++ JavaScript/Javascript Assíncrono/cat api/script.js
@@ -0,0 +1,19 @@
+const BASE_URL = 'https://api.thecatapi.com/v1/images/search'; 
+
+const getCats = async () => {
+    try{
+        const data = await fetch(BASE_URL);
+        const json = await data.json();
+        return json[0].url;
+
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+const loadImg = async () => {
+    const img = document.getElementById('cat');
+    img.src = await getCats();
+
+}
+loadImg();
