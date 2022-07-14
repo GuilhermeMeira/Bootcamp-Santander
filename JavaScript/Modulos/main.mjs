29810c8ba0b82447f83babcfb8c5d85df64503b0
Guilherme <guilhermemsantana16@gmail.com>
2022-07-14 13:06:21 -0300

    Módulos

---
+++ JavaScript/Modulos/main.mjs
@@ -0,0 +1,6 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+
+console.log(mostraIdade('João', 26))
+console.log(mostraCidade('Gui', 'Salvador'))
+console.log(mostraHobby('Jeferson', 'jogar futebol'))
